2a34931f27b8345fdded0d47f49de8d97995b295
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-03-25 21:28:01 -0300

    Add calcula_percentil

--- utils.py
+++ utils.py
@@ -57,90 +57,28 @@ def calcular_dmeda(conjunto):
     return calcular_mediana(novo_conjunto)
 
 
-# def calcular_quartil_01(conjunto, debug=True):
-#     """
-#     Implementação do cálculo de quartil
-
-#     no octave: quantile(conjunto, [0.25, 0.5, 0.75])
-#     """
-#     tamanho = len(conjunto)
-#     conjunto_ordenado = sorted(conjunto)
-#     posicao_q1 = math.ceil((tamanho + 1) * (1 / 4)) - 1
-#     if debug:
-#         print((tamanho + 1) * (1 / 4))
-
-#     posicao_q1_menor = conjunto_ordenado[posicao_q1 - 1]
-#     posicao_q1_maior = conjunto_ordenado[posicao_q1]
-#     if debug:
-#         print("(0.25 * {}) + (0.75 * {})".format(posicao_q1_menor, posicao_q1_maior))
-
-#     # média ponderada
-#     q1 = (0.25 * posicao_q1_menor) + (0.75 * posicao_q1_maior)
-#     q2 = calcular_mediana(conjunto_ordenado)
-
-#     posicao_q3 = math.ceil((tamanho + 1) * (3 / 4)) - 1
-#     if debug:
-#         print((tamanho + 1) * (3 / 4))
-
-#     posicao_q3_menor = conjunto_ordenado[posicao_q3 - 1]
-#     posicao_q3_maior = conjunto_ordenado[posicao_q3]
-#     if debug:
-#         print("(0.75 * {}) + (0.25 * {})".format(posicao_q3_menor, posicao_q3_maior))
-
-#     # média ponderada
-#     q3 = (0.75 * posicao_q3_menor) + (0.25 * posicao_q3_maior)
-#     return q1, q2, q3
-
-
-# def calcular_quartil_02(conjunto, debug=True):
-#     """
-#     Implementação do cálculo de quartil
-
-#     no octave: quantile(conjunto, [0.25, 0.5, 0.75])
-#     """
-#     tamanho = len(conjunto)
-#     conjunto_ordenado = sorted(conjunto)
-
-#     posicao_q1 = math.ceil(tamanho * (1 / 4)) - 1
-#     q1 = conjunto_ordenado[posicao_q1]
-
-#     q2 = calcular_mediana(conjunto_ordenado)
-
-#     posicao_q3 = math.ceil(tamanho * (3 / 4)) - 1
-#     q3 = conjunto_ordenado[posicao_q3]
-#     return q1, q2, q3
-
-
-def calcular_quartil(conjunto, debug=True):
+def calcula_percentil(conjunto, p):
     """
-    Implementação do cálculo de quartil
+    Implementação do cálculo de percentil
+    também pode ser usado para cálculo de quartil (0.25, 0.5 e 0.75)
 
-    no octave: quantile(conjunto, [0.25, 0.5, 0.75])
+    conjunto é uma lista de elementos
+    p deve ser um valor percentual já calculado (0.25, 0.4 etc)
     """
-    tamanho = len(conjunto)
     conjunto_ordenado = sorted(conjunto)
-    posicao_q1 = math.ceil((tamanho + 1) * (1 / 4)) - 1
-    if debug:
-        print((tamanho + 1) * (1 / 4))
 
-    posicao_q1_menor = conjunto_ordenado[posicao_q1 - 1]
-    posicao_q1_maior = conjunto_ordenado[posicao_q1]
-    if debug:
-        print("({} + {}) / 2.0".format(posicao_q1_menor, posicao_q1_maior))
+    if p == 0:
+        return conjunto_ordenado[0]
+    elif p == 1:
+        return conjunto_ordenado[-1]
 
-    # média comum
-    q1 = (posicao_q1_menor + posicao_q1_maior) / 2.0
-    q2 = calcular_mediana(conjunto_ordenado)
+    m = len(conjunto_ordenado)
+    k = m * p
+    k = k - 1  # ajustando lista python
 
-    posicao_q3 = math.ceil((tamanho + 1) * (3 / 4)) - 1
-    if debug:
-        print((tamanho + 1) * (3 / 4))
+    if not k.is_integer():
+        k = int(math.ceil(k))
+        return conjunto_ordenado[k]
 
-    posicao_q3_menor = conjunto_ordenado[posicao_q3 - 1]
-    posicao_q3_maior = conjunto_ordenado[posicao_q3]
-    if debug:
-        print("({} + {}) / 2.0".format(posicao_q3_menor, posicao_q3_maior))
-
-    # média comum
-    q3 = (posicao_q3_menor + posicao_q3_maior) / 2.0
-    return q1, q2, q3
+    k = int(k)
+    return (conjunto_ordenado[k] + conjunto_ordenado[k + 1]) / 2.
